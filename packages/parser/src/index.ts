99134fe8ee3f89f66c3bfc1b4f62c7ea3d5bb4bc
lollipop-onl <lollipop@simochee.net>
2025-06-25 11:54:50 +0900

    Revert "feat: add @simochee/passport-mrz-parser package"
    This reverts commit 34e225a2090de6a66cfda2bdd88390693679cd28.

--- packages/parser/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { MRZ_ALLOWED_CHARS, TD3_LINE_LENGTH, TD3_LINES } from "./constants";
-export { parseMRZ } from "./parser";
-export type { ParseError, ParseOptions, ParseResult } from "./types";
-export { calculateCheckDigit } from "./validator";
